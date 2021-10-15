84c841eed2fc9fa25a9fba94b1b39f59f389af89
Irina <loabanova.irina.96@mail.ru>
2021-10-15 16:29:11 +0300

    fix

--- solution/index.js
+++ solution/index.js
@@ -12,29 +12,33 @@ module.exports = function (Homework) {
     };
   }
 
+  async function reduce(asyncArray, fn, initialValue, cb) {
+    const getLength = promisify(asyncArray.length);
+    const getItem = promisify(asyncArray.get);
+    const getSum = promisify(add);
+    const compare = promisify(less);
+    const fnPromisse = promisify(fn);
+
+    let result = initialValue;
+    let i = 0;
+    let check = true;
+    const length = await getLength().then((res) => res);
+
+    while (check) {
+      const current = await getItem(i).then((res) => res);
+
+      result = await fnPromisse(result, current, i, asyncArray).then(
+        (res) => res
+      );
+
+      i = await getSum(i, 1).then((res) => res);
+      check = await compare(i, length).then((res) => res);
+    }
+
+    return cb(result);
+  }
+
   return (array, fn, initialValue, cb) => {
-    return async function () {
-      const getLength = promisify(array.length);
-      const getItem = promisify(array.get);
-      const getSum = promisify(add);
-      const compare = promisify(less);
-      const fnPromisse = promisify(fn);
-
-      let result = initialValue;
-      let i = 0;
-      let check = true;
-      const length = await getLength().then((res) => res);
-
-      while (check) {
-        const current = await getItem(i).then((res) => res);
-
-        result = await fnPromisse(result, current, i, array).then((res) => res);
-
-        i = await getSum(i, 1).then((res) => res);
-        check = await compare(i, length).then((res) => res);
-      }
-
-      return cb(result);
-    };
+    reduce(array, fn, initialValue, cb);
   };
 };
